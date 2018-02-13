417c2713a2b0b45738a19ccbb85fd768a41194b6
Alan Kelly <alan.kelly1@mycit.ie>
2018-02-13 22:42:49 +0000

    added gui

--- FYP/Form1.Designer.cs
+++ FYP/Form1.Designer.cs
@@ -35,23 +35,25 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.netIp1a = new System.Windows.Forms.NumericUpDown();
-            this.netIp1d = new System.Windows.Forms.NumericUpDown();
-            this.netIp1c = new System.Windows.Forms.NumericUpDown();
             this.netIp1b = new System.Windows.Forms.NumericUpDown();
-            this.subnet1b = new System.Windows.Forms.NumericUpDown();
-            this.subnet1c = new System.Windows.Forms.NumericUpDown();
+            this.netIp1c = new System.Windows.Forms.NumericUpDown();
+            this.netIp1d = new System.Windows.Forms.NumericUpDown();
             this.subnet1d = new System.Windows.Forms.NumericUpDown();
+            this.subnet1c = new System.Windows.Forms.NumericUpDown();
+            this.subnet1b = new System.Windows.Forms.NumericUpDown();
             this.subnet1a = new System.Windows.Forms.NumericUpDown();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.netIp1a)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.netIp1d)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.netIp1c)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.netIp1b)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subnet1b)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subnet1c)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.netIp1c)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.netIp1d)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.subnet1d)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subnet1c)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subnet1b)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.subnet1a)).BeginInit();
             this.SuspendLayout();
             // 
@@ -126,18 +128,29 @@
             this.netIp1a.Name = "netIp1a";
             this.netIp1a.Size = new System.Drawing.Size(43, 24);
             this.netIp1a.TabIndex = 18;
+            this.netIp1a.Value = new decimal(new int[] {
+            192,
+            0,
+            0,
+            0});
             // 
-            // netIp1d
+            // netIp1b
             // 
-            this.netIp1d.Location = new System.Drawing.Point(340, 70);
-            this.netIp1d.Maximum = new decimal(new int[] {
+            this.netIp1b.Location = new System.Drawing.Point(196, 70);
+            this.netIp1b.Maximum = new decimal(new int[] {
             255,
             0,
             0,
             0});
-            this.netIp1d.Name = "netIp1d";
-            this.netIp1d.Size = new System.Drawing.Size(43, 24);
-            this.netIp1d.TabIndex = 19;
+            this.netIp1b.Name = "netIp1b";
+            this.netIp1b.Size = new System.Drawing.Size(43, 24);
+            this.netIp1b.TabIndex = 19;
+            this.netIp1b.Value = new decimal(new int[] {
+            168,
+            0,
+            0,
+            0});
+            this.netIp1b.ValueChanged += new System.EventHandler(this.netIp1b_ValueChanged);
             // 
             // netIp1c
             // 
@@ -151,29 +164,31 @@
             this.netIp1c.Size = new System.Drawing.Size(43, 24);
             this.netIp1c.TabIndex = 20;
             // 
-            // netIp1b
+            // netIp1d
             // 
-            this.netIp1b.Location = new System.Drawing.Point(196, 70);
-            this.netIp1b.Maximum = new decimal(new int[] {
+            this.netIp1d.Location = new System.Drawing.Point(340, 69);
+            this.netIp1d.Maximum = new decimal(new int[] {
             255,
             0,
             0,
             0});
-            this.netIp1b.Name = "netIp1b";
-            this.netIp1b.Size = new System.Drawing.Size(43, 24);
-            this.netIp1b.TabIndex = 21;
+            this.netIp1d.Name = "netIp1d";
+            this.netIp1d.Size = new System.Drawing.Size(43, 24);
+            this.netIp1d.TabIndex = 21;
+            this.netIp1d.ValueChanged += new System.EventHandler(this.netIp1d_ValueChanged);
             // 
-            // subnet1b
+            // subnet1d
             // 
-            this.subnet1b.Location = new System.Drawing.Point(601, 69);
-            this.subnet1b.Maximum = new decimal(new int[] {
+            this.subnet1d.Location = new System.Drawing.Point(745, 69);
+            this.subnet1d.Maximum = new decimal(new int[] {
             255,
             0,
             0,
             0});
-            this.subnet1b.Name = "subnet1b";
-            this.subnet1b.Size = new System.Drawing.Size(43, 24);
-            this.subnet1b.TabIndex = 28;
+            this.subnet1d.Name = "subnet1d";
+            this.subnet1d.Size = new System.Drawing.Size(43, 24);
+            this.subnet1d.TabIndex = 28;
+            this.subnet1d.ValueChanged += new System.EventHandler(this.subnet1d_ValueChanged);
             // 
             // subnet1c
             // 
@@ -186,18 +201,29 @@
             this.subnet1c.Name = "subnet1c";
             this.subnet1c.Size = new System.Drawing.Size(43, 24);
             this.subnet1c.TabIndex = 27;
-            // 
-            // subnet1d
-            // 
-            this.subnet1d.Location = new System.Drawing.Point(745, 69);
-            this.subnet1d.Maximum = new decimal(new int[] {
+            this.subnet1c.Value = new decimal(new int[] {
             255,
             0,
             0,
             0});
-            this.subnet1d.Name = "subnet1d";
-            this.subnet1d.Size = new System.Drawing.Size(43, 24);
-            this.subnet1d.TabIndex = 26;
+            // 
+            // subnet1b
+            // 
+            this.subnet1b.Location = new System.Drawing.Point(601, 69);
+            this.subnet1b.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.subnet1b.Name = "subnet1b";
+            this.subnet1b.Size = new System.Drawing.Size(43, 24);
+            this.subnet1b.TabIndex = 26;
+            this.subnet1b.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.subnet1b.ValueChanged += new System.EventHandler(this.subnet1b_ValueChanged);
             // 
             // subnet1a
             // 
@@ -210,6 +236,11 @@
             this.subnet1a.Name = "subnet1a";
             this.subnet1a.Size = new System.Drawing.Size(43, 24);
             this.subnet1a.TabIndex = 25;
+            this.subnet1a.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
             // 
             // label6
             // 
@@ -241,19 +272,40 @@
             this.label8.TabIndex = 22;
             this.label8.Text = ".";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Select Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(205, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 24);
+            this.textBox1.TabIndex = 31;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1078, 344);
-            this.Controls.Add(this.subnet1b);
-            this.Controls.Add(this.subnet1c);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.subnet1d);
+            this.Controls.Add(this.subnet1c);
+            this.Controls.Add(this.subnet1b);
             this.Controls.Add(this.subnet1a);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.netIp1b);
-            this.Controls.Add(this.netIp1c);
             this.Controls.Add(this.netIp1d);
+            this.Controls.Add(this.netIp1c);
+            this.Controls.Add(this.netIp1b);
             this.Controls.Add(this.netIp1a);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -264,12 +316,12 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.netIp1a)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.netIp1d)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.netIp1c)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.netIp1b)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subnet1b)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subnet1c)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.netIp1c)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.netIp1d)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.subnet1d)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subnet1c)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subnet1b)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.subnet1a)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -284,16 +336,18 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.NumericUpDown netIp1a;
-        private System.Windows.Forms.NumericUpDown netIp1d;
-        private System.Windows.Forms.NumericUpDown netIp1c;
         private System.Windows.Forms.NumericUpDown netIp1b;
-        private System.Windows.Forms.NumericUpDown subnet1b;
-        private System.Windows.Forms.NumericUpDown subnet1c;
+        private System.Windows.Forms.NumericUpDown netIp1c;
+        private System.Windows.Forms.NumericUpDown netIp1d;
         private System.Windows.Forms.NumericUpDown subnet1d;
+        private System.Windows.Forms.NumericUpDown subnet1c;
+        private System.Windows.Forms.NumericUpDown subnet1b;
         private System.Windows.Forms.NumericUpDown subnet1a;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- FYP/Form1.cs
+++ FYP/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.NetworkInformation;
@@ -12,14 +13,18 @@ using System.Diagnostics;
 using System.Net;
 using System.Threading;
 using System.Net.Sockets;
+using System.Globalization;
 
 namespace FYP_10_2_18
 {
     public partial class Form1 : Form
     {
+        static CountdownEvent countdown;
         static int upCount = 0;
+        static object lockObj = new object();
         const bool resolveNames = true;
         String s;
+        String path;
         String netIp = "192.168.0.";
 
     public Form1()
@@ -28,14 +33,58 @@ namespace FYP_10_2_18
         }
 
 
+        private void ScanNetwork_Click_1(object sender, EventArgs e)
+        {
+            if (path != null)
+            {
+                upCount = 0;
+                countdown = new CountdownEvent(1);
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                string ipBase = netIp1a.Value.ToString() + "." + netIp1b.Value.ToString() + "." + netIp1c.Value.ToString() + "." + netIp1d.Value.ToString();
+                Trace.WriteLine("\nipBase = " + ipBase + "\n");
+                DateTime localDate = DateTime.Now;
+                var culture = new CultureInfo("en-GB");
+                writeToFile(localDate.ToString(culture));
+
+                ipBase = ipBase.Substring(0, (ipBase.LastIndexOf(".") + 1));
+                Trace.WriteLine("\nipBase = " + ipBase + "\n");
+                for (int i = 1; i < 255; i++)
+                {
+                    string ip = ipBase + i.ToString();
+
+                    Ping p = new Ping();
+                    p.PingCompleted += new PingCompletedEventHandler(pingCompleted);
+
+                    p.SendAsync(ip, 100, ip);
+                }
+                string message = "Network scan is now complete";
+                string caption = "Scan Successful";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                result = MessageBox.Show(message, caption, buttons);
+            }
+            else
+            {
+                string message = "Please enter a path to save the text file";
+                string caption = "No Path";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                result = MessageBox.Show(message, caption, buttons);
+            }
+
+
+        }
+
         public void pingCompleted(object sender, PingCompletedEventArgs e)
         {
-            
             string ip = (string)e.UserState;
             if (e.Reply != null && e.Reply.Status == IPStatus.Success)
             {
+                lock (lockObj)
+                {
                     upCount++;
-                
+                }
                 if (resolveNames)
                 {
                     string name;
@@ -46,156 +95,32 @@ namespace FYP_10_2_18
                     }
                     catch (SocketException ex)
                     {
-                        name = "Unknown Host Name";
+                        name = "?";
                     }
-                    s = $"host {upCount}: {ip} ({name}) is up: ({e.Reply.RoundtripTime} ms)";
-                    writeToFile(s);
+                    //Console.WriteLine("{0} ({1}) is up: ({2} ms)", ip, name, e.Reply.RoundtripTime);
+                    writeToFile($"Host {upCount} = {ip} ({name}) is up: ({e.Reply.RoundtripTime} ms)");
                 }
                 else
                 {
-                    s = $"{ip} is up: ({e.Reply.RoundtripTime} ms))";
-                    writeToFile(s);
+                    //Console.WriteLine("{0} is up: ({1} ms)", ip, e.Reply.RoundtripTime);
+                    writeToFile($"{ip} is up: ({e.Reply.RoundtripTime} ms)");
                 }
             }
             else if (e.Reply == null)
             {
-                s = $"Pinging {ip} failed. (Null Reply object?)";
-                writeToFile(s);
+                //Console.WriteLine("Pinging {0} failed. (Null Reply object?)", ip);
+                writeToFile($"Pinging {ip} failed. (Null Reply object?)");
             }
+            
         }
 
         public void writeToFile(String word)
         {
             using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(@"C:\Users\Grim\fyptestnw.txt", true))
+            new System.IO.StreamWriter(@path + "/sample.txt", true))
             {
                 file.WriteLine(word);
             }
-        }
-        
-        private void ValIp(TextBox tbox)
-        {
-
-            // Checks the value of the text.
-            int count = tbox.Text.Count(f => f == '.'); 
-            if (count != 3)
-            {
-
-                // Initializes the variables to pass to the MessageBox.Show method.
-
-                string message = "you have not enetered a correct IP address. Please reenter IP address";
-                string caption = "Error Detected in Input";
-                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
-                DialogResult result;
-
-                // Displays the MessageBox.
-
-                result = MessageBox.Show(message, caption, buttons);
-
-                if (result == System.Windows.Forms.DialogResult.No)
-                {
-
-                    // Closes the parent form.
-                    this.Close();
-
-                }
-
-            }
-
-        }
-
-        private void ValSubnet(TextBox tbox)
-        {
-
-            // Checks the value of the text.
-            int count = tbox.Text.Count(f => f == '.');
-            int zeros = tbox.Text.Count(f => f == '0');
-            if (count != 3)
-            {
-
-                // Initializes the variables to pass to the MessageBox.Show method.
-
-                string message = "you have not enetered a correct subnet address. Please reenter Subnet";
-                string caption = "Error Detected in Input";
-                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
-                DialogResult result;
-
-                // Displays the MessageBox.
-
-                result = MessageBox.Show(message, caption, buttons);
-
-                if (result == System.Windows.Forms.DialogResult.No)
-                {
-                    // Closes the parent form.
-                    this.Close();
-
-                }
-
-            }
-            if (zeros == 1)
-            {
-                netIp = tbox.Text.Substring(0, (tbox.Text.LastIndexOf(".") + 1));
-            }
-            if (zeros == 2)
-            {
-                netIp = tbox.Text.Substring(0, (tbox.Text.LastIndexOf(".") + 1));
-            }
-
-        }
-
-        private void ScanNetwork_Click_1(object sender, EventArgs e)
-        {
-
-
-            string ipBase = netIp1a.Value.ToString() + "." + netIp1b.Value.ToString() + "." + netIp1c.Value.ToString() + "." + netIp1d.Value.ToString();
-            //ipBase = ipBase.Substring(0, (ipBase.LastIndexOf(".") + 1));
-
-            if(subnet1d.Value == 0)
-            {
-                if(subnet1c.Value == 0)
-                {
-                    if (subnet1a.Value == 255)
-                    {
-
-                        ipBase = ipBase.Substring(0, (ipBase.LastIndexOf(".")));
-
-                        ipBase = ipBase.Substring(0, (ipBase.LastIndexOf(".") + 1));
-                        
-                        for (int i = 1; i < 255; i++)
-                        {
-                            for (int f = 1; f < 255; f++)
-                            {
-                                string ip = ipBase + i.ToString() + "." + f.ToString();
-
-                                Ping p = new Ping();
-                                p.PingCompleted += new PingCompletedEventHandler(pingCompleted);
-                                p.SendAsync(ip, ip);
-                                
-                            }
-                        }
-                    }
-                }
-                else if (subnet1c.Value == 255)
-                {
-                    if (subnet1b.Value == 255)
-                    {
-                        if (subnet1a.Value == 255)
-                        {
-                            ipBase = ipBase.Substring(0, (ipBase.LastIndexOf(".") + 1));
-                            for (int i = 1; i < 255; i++)
-                            {
-                                string ip = ipBase + i.ToString();
-
-                                Ping p = new Ping();
-                                p.PingCompleted += new PingCompletedEventHandler(pingCompleted);
-                                p.SendAsync(ip, 100, ip);
-                            }
-                        }
-                    }
-                }
-            }
-            
-
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -267,7 +192,125 @@ namespace FYP_10_2_18
         {
 
         }
-        
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void ValIp(TextBox tbox)
+        {
+
+            // Checks the value of the text.
+            int count = tbox.Text.Count(f => f == '.');
+            if (count != 3)
+            {
+
+                // Initializes the variables to pass to the MessageBox.Show method.
+
+                string message = "you have not enetered a correct IP address. Please reenter IP address";
+                string caption = "Error Detected in Input";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+
+                // Displays the MessageBox.
+
+                result = MessageBox.Show(message, caption, buttons);
+
+                if (result == System.Windows.Forms.DialogResult.No)
+                {
+
+                    // Closes the parent form.
+                    this.Close();
+
+                }
+
+            }
+
+        }
+
+        private void ValSubnet(TextBox tbox)
+        {
+
+            // Checks the value of the text.
+            int count = tbox.Text.Count(f => f == '.');
+            int zeros = tbox.Text.Count(f => f == '0');
+            if (count != 3)
+            {
+
+                // Initializes the variables to pass to the MessageBox.Show method.
+
+                string message = "you have not enetered a correct subnet address. Please reenter Subnet";
+                string caption = "Error Detected in Input";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+
+                if (result == System.Windows.Forms.DialogResult.No)
+                {
+                    // Closes the parent form.
+                    this.Close();
+
+                }
+
+            }
+            if (zeros == 1)
+            {
+                netIp = tbox.Text.Substring(0, (tbox.Text.LastIndexOf(".") + 1));
+            }
+            if (zeros == 2)
+            {
+                netIp = tbox.Text.Substring(0, (tbox.Text.LastIndexOf(".") + 1));
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    Trace.Write(fbd.SelectedPath);
+                    path = fbd.SelectedPath;
+                    Trace.Write(path);
+                    textBox1.Text = path + "\\sample.txt";
+                }
+            }
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void netIp1b_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void netIp1d_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subnet1b_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subnet1d_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
     
 }
